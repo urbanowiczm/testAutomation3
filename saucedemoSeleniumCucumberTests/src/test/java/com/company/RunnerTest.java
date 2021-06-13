8f40c71b3da9bb4124bc5232caa2250b97961eaf
Maciej Urbanowicz <urbanowicz.maciej@gmail.com>
2021-06-13 22:57:42 +0200

    add reports

--- saucedemoSeleniumCucumberTests/src/test/java/com/company/RunnerTest.java
+++ saucedemoSeleniumCucumberTests/src/test/java/com/company/RunnerTest.java
@@ -5,6 +5,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"}, features = "src/test/resources")
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json",
+        "junit:target/cucumber-reports/Cucumber.xml",
+        "html:target/cucumber-report.html"}, features = "src/test/resources")
 public class RunnerTest {
 }
--- saucedemoSeleniumCucumberTests/src/test/java/com/company/steps/MainPageSteps.java
+++ saucedemoSeleniumCucumberTests/src/test/java/com/company/steps/MainPageSteps.java
@@ -1,20 +1,10 @@
 package com.company.steps;
 
 import com.company.pages.InventoryPage;
-import com.company.utils.DriverSingleton;
 import com.company.pages.MainPage;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-
-import java.io.File;
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
